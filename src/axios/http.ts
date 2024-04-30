25016f032f8cab28fa17f54d2080ac4d8620476b
jaeyeon kim <jakind@naver.com>
2024-04-30 09:58:55 +0900

    feat: 채팅 목록 Polling 기능 구현

--- src/axios/http.ts
+++ src/axios/http.ts
@@ -1,10 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { http } from './instances'
 
-export const useChatQuery = () => {
+export const useChatsQuery = () => {
     const { data: chats, ...rest } = useSuspenseQuery({
         queryKey: ['chat'], // cookie에 들어있는 값으로 캐싱하는 것이 좋을 것 같은데, 이 부분 같이 상의해봐야 할 것 같아요
-        queryFn: () => getChats,
+        queryFn: () => getChats(),
     })
     return {
         chats,
--- src/pages/Day.tsx
+++ src/pages/Day.tsx
@@ -1,15 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import { VariablesCSS } from "../styles/VariablesCSS";
-import AppContainerCSS from "../components/layout/AppContainerCSS";
-import { useEffect, useState } from "react";
-import TopDay from "../components/top/TopDay";
-import ChatGroup from "../components/chat/ChatGroup";
-import ModalContainer from "../components/modal/ModalContainer";
-import Vote from "../components/modal/Vote";
-import ViewRole from "../components/modal/ViewRole";
-import NoticeRole from "../components/modal/NoticeRole";
-import NoticeDead from "../components/modal/NoticeDead";
+import { css } from '@emotion/react'
+import { VariablesCSS } from '../styles/VariablesCSS'
+import AppContainerCSS from '../components/layout/AppContainerCSS'
+import { useEffect, useState } from 'react'
+import TopDay from '../components/top/TopDay'
+import ChatGroup from '../components/chat/ChatGroup'
+import ModalContainer from '../components/modal/ModalContainer'
+import Vote from '../components/modal/Vote'
+import ViewRole from '../components/modal/ViewRole'
+import NoticeRole from '../components/modal/NoticeRole'
+import NoticeDead from '../components/modal/NoticeDead'
+import { useChatQuery, useChatsQuery } from '../axios/http'
 
 export default function Day() {
     const gameMessage = css`
@@ -17,11 +18,11 @@ export default function Day() {
         justify-content: center;
         align-items: center;
         height: calc(100% - ${VariablesCSS.top});
-        font-family: "Cafe24Ssurround", sans-serif;
+        font-family: 'Cafe24Ssurround', sans-serif;
         font-size: 24px;
         color: ${VariablesCSS.day};
         animation: smoothshow 0.8s;
-    `;
+    `
 
     const middle = css`
         height: calc(100% - ${VariablesCSS.top} - 55px - 20px);
@@ -31,7 +32,7 @@ export default function Day() {
         &::-webkit-scrollbar {
             display: none;
         }
-    `;
+    `
 
     const chatInput = css`
         box-sizing: border-box;
@@ -40,7 +41,7 @@ export default function Day() {
         margin-right: 4px;
         padding: 11px 20px;
         font-size: 16px;
-        font-family: "KCC-Hanbit", sans-serif;
+        font-family: 'KCC-Hanbit', sans-serif;
         color: ${VariablesCSS.day};
         background-color: rgba(255, 255, 255, 0.2);
         border: 3px solid #ffffff;
@@ -54,42 +55,43 @@ export default function Day() {
             outline: 3px solid #ffffff;
             background-color: rgba(255, 255, 255, 0.4);
         }
-    `;
+    `
 
     /* 공지모달 */
-    const [role, setRole] = useState("mafia");
+    const [role, setRole] = useState('mafia')
 
-    const [descriptionTime, setDescriptionTime] = useState(true);
-    const [noticeTime, setNoticeTime] = useState(false);
+    const [descriptionTime, setDescriptionTime] = useState(true)
+    const [noticeTime, setNoticeTime] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
-            setDescriptionTime(false);
-            setNoticeTime(true);
-        }, 3000);
+            setDescriptionTime(false)
+            setNoticeTime(true)
+        }, 3000)
 
         setTimeout(() => {
-            setNoticeTime(false);
-        }, 6000);
-    }, []);
+            setNoticeTime(false)
+        }, 6000)
+    }, [])
 
     /* 시간 */
-    const [time, setTime] = useState(0);
+    const [time, setTime] = useState(0)
 
-    const [round, setRound] = useState(1);
-    const [yesterdayDead, setYesterdayDead] = useState(false);
+    const [round, setRound] = useState(1)
+    const [yesterdayDead, setYesterdayDead] = useState(false)
 
     /* 내가 살아있는지 */
-    const [isAlive, setIsAlive] = useState(true);
+    const [isAlive, setIsAlive] = useState(true)
 
     /* 미리 투표하기 */
-    const [openModal, setOpenModal] = useState(false);
+    const [openModal, setOpenModal] = useState(false)
     const onOpenModal = () => {
-        setOpenModal(!openModal);
-    };
+        setOpenModal(!openModal)
+    }
 
     // 모두 미리 투표했는지
-    const [allVote, setAllVote] = useState(false);
+    const [allVote, setAllVote] = useState(false)
+    const { chats } = useChatsQuery()
 
     return (
         <AppContainerCSS background="day">
@@ -101,9 +103,14 @@ export default function Day() {
 
                     <>
                         <div css={middle}>
-                            <ChatGroup />
-                            <ChatGroup />
-                            <ChatGroup />
+                            {/* 실제로 여기에다가 ChatGroup이 추가되는 형태로 이루어져야함 */}
+                            {chats.map((chat: Chat) => (
+                                <ChatGroup
+                                    name={chat.name}
+                                    content={chat.content}
+                                    owner={chat.owner}
+                                />
+                            ))}
                         </div>
 
                         {/* 살아있는 경우에만 input창이 보인다. */}
@@ -130,7 +137,7 @@ export default function Day() {
                                     type="submit"
                                     value="전송"
                                     css={css`
-                                        font-family: "Cafe24Ssurround", sans-serif;
+                                        font-family: 'Cafe24Ssurround', sans-serif;
                                         padding: 18px 14px;
                                         font-size: 16px;
                                         color: ${VariablesCSS.day};
@@ -174,5 +181,5 @@ export default function Day() {
                 </div>
             )}
         </AppContainerCSS>
-    );
+    )
 }
