b802796f9a179577e1ad7667c5cda5821a2c25d3
jaeyeon kim <jakind@naver.com>
2024-05-14 11:23:33 +0900

    feat: 경찰 밤 화면 구현 완료

--- src/components/job/PoliceNight.tsx
+++ src/components/job/PoliceNight.tsx
@@ -6,8 +6,8 @@ import PlayerGrid from '../player/PlayerGrid'
 import { Player } from '../../type'
 import PlayerNight from '../player/PlayerNight'
 import ModalContainer from '../modal/ModalContainer'
+import { useEffect, useState } from 'react'
 import InvestResult from '../modal/InvestResult'
-import { useState } from 'react'
 
 interface Props {
     isAlive: boolean
@@ -21,24 +21,49 @@ export const PoliceNight = ({ players, isAlive }: Props) => {
         text-align: center;
         color: ${VariablesCSS.light};
     `
+    const middle = css`
+        height: calc(100% - ${VariablesCSS.top});
+        overflow: scroll;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    `
+    const [check, setCheck] = useState<number>(-1)
     const [openModal, setOpenModal] = useState<boolean>(false)
+    useEffect(() => {
+        (async () => {
+            if (check === -1) {
+                return
+            }
+            setOpenModal(true)
+        })()
+    }, [check, players])
 
     return (
         <AppContainerCSS background="night">
-            <>
+            <div css={middle}>
                 <div css={description}>
                     {isAlive ? '마피아로 의심되는 사람을 조사하세요.' : '밤이 지나가고 있습니다..'}
                 </div>
                 <PlayerGrid>
                     {players.map((player, i) => (
-                        <PlayerNight player={player} key={i + 1} index={i + 1} myJob={'POLICE'} />
+                        <PlayerNight
+                            player={player}
+                            key={i + 1}
+                            index={i + 1}
+                            myJob={'POLICE'}
+                            {...(isAlive && { nowVoteResult: check })}
+                            {...(isAlive && { setCheck: setCheck })}
+                        />
                     ))}
                 </PlayerGrid>
                 {/* 경찰: 조사하기 */}
                 <ModalContainer isOpen={openModal}>
-                    <InvestResult />
+                    <InvestResult target={players[check - 1]?.name} />
                 </ModalContainer>
-            </>
+            </div>
         </AppContainerCSS>
     )
 }
