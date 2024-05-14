096338884931bdc7412782f7c3a8530c0147cea0
jaeyeon kim <jakind@naver.com>
2024-05-14 10:58:03 +0900

    feat: 의사 기능 완료

--- src/components/job/DoctorNight.tsx
+++ src/components/job/DoctorNight.tsx
@@ -5,6 +5,8 @@ import AppContainerCSS from '../layout/AppContainerCSS'
 import { VariablesCSS } from '../../styles/VariablesCSS'
 import PlayerGrid from '../player/PlayerGrid'
 import PlayerNight from '../player/PlayerNight'
+import { useEffect, useState } from 'react'
+import { postSkill } from '../../axios/http'
 
 interface Props {
     isAlive: boolean
@@ -18,18 +20,47 @@ export const DoctorNight = ({ players, isAlive }: Props) => {
         text-align: center;
         color: ${VariablesCSS.light};
     `
+    const middle = css`
+        height: calc(100% - ${VariablesCSS.top});
+        overflow: scroll;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    `
+    const [check, setCheck] = useState<number>(-1)
+    useEffect(() => {
+        (async () => {
+            if (check === -1) {
+                return
+            }
+            players.forEach(async (player, i) => {
+                if (check === i + 1) {
+                    await postSkill({ target: player.name })
+                }
+            })
+        })()
+    }, [check, players])
     return (
         <AppContainerCSS background="night">
-            <>
+            <div css={middle}>
                 <div css={description}>
                     {isAlive ? '오늘밤 살릴 사람을 지목해주세요.' : '밤이 지나가고 있습니다..'}
                 </div>
                 <PlayerGrid>
                     {players.map((player, i) => (
-                        <PlayerNight player={player} key={i + 1} index={i + 1} myJob={'DOCTOR'} />
+                        <PlayerNight
+                            player={player}
+                            key={i + 1}
+                            index={i + 1}
+                            myJob={'DOCTOR'}
+                            {...(isAlive && { nowVoteResult: check })}
+                            {...(isAlive && { setCheck: setCheck })}
+                        />
                     ))}
                 </PlayerGrid>
-            </>
+            </div>
         </AppContainerCSS>
     )
 }
