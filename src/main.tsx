a5c4e1ca8e93a833469529ab108be3dedef136a8
jaeyeon kim <jakind@naver.com>
2024-05-28 13:35:58 +0900

    refactor: HashRouter 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RecoilRoot } from 'recoil'
+import { HashRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <RecoilRoot>
             <QueryClientProvider client={queryClient}>
-                <App />
+                <HashRouter>
+                    <App />
+                </HashRouter>
             </QueryClientProvider>
         </RecoilRoot>
     </React.StrictMode>
